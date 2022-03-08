6a92323b937a52a3aa77f6b3e29753ccb1592f1b
Kate Pearce <74689733+kjpearce@users.noreply.github.com>
2022-03-08 11:09:04 -0500

    Fixed bug in SuccessCheck.m

--- SuccessCheck.m
+++ SuccessCheck.m
@@ -26,24 +26,35 @@ function [abs_err, rel_err, cond_S, cond_S1] = SuccessCheck(S, P, k, SingVals)
    SingVals = nonzeros(SingVals);
    %%% check criteria 2.4: |Sig(S)_k - Sig(S1)_k|
    S_perm = S(:,P);
-   S1 = S_perm(:,1:k);
-   S2 = S_perm(:,k+1:end);
-   
-   SingVals_S1   = svd(S1, 0);
-   SigS1_k = SingVals_S1(k);
-   Sig_k   = SingVals(k);
-   
-   cond_S = cond(S);
-   cond_S1 = cond(S1);
 
-   abs_err(1) = abs(SigS1_k - Sig_k);
-   rel_err(1) = abs_err(1)/Sig_k; 
+   if k == length(P)
+       S1 = S_perm;
+       cond_S = cond(S1);
+       cond_S1 = cond(S1);
+
+   else
+       S1 = S_perm(:,1:k);
+       S2 = S_perm(:,k+1:end);
+       
+       SingVals_S1   = svd(S1, 0);
+       SigS1_k = SingVals_S1(k);
+       Sig_k   = SingVals(k);
+       
+       cond_S = cond(S);
+       cond_S1 = cond(S1);
+    
+       abs_err(1) = abs(SigS1_k - Sig_k);
+       %rel_err(1) = abs_err(1)/Sig_k; 
+       rel_err(1) = SigS1_k/Sig_k; %%% gamma_1
+       
+       %%% check criteria 2.5 | ||(I-S1*pinv(S1))*S|| - Sig(S)_{k+1}|
+       Sig_kp1  = SingVals(k+1);
+       X = S1\S2;
+       crit_norm = norm(S2-S1*X, 2);
+       abs_err(2) = crit_norm; %%% two norm of residual for least squares problem
+       rel_err(2) = abs_err(2)/Sig_kp1; %%% gamma_2
+
+   end
    
-   %%% check criteria 2.5 | ||(I-S1*pinv(S1))*S|| - Sig(S)_{k+1}|
-   Sig_kp1  = SingVals(k+1);
-   X = S1\S2;
-   crit_norm = norm(S2-S1*X, 2);
-   abs_err(2) = crit_norm; %%% two norm of residual of least squares problem
-   rel_err(2) = abs_err(2)/Sig_kp1;
    
 end
