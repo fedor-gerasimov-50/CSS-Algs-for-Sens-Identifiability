99dea05914238e6eabf6eafbe9bfdeda889562da
Kate Pearce <74689733+kjpearce@users.noreply.github.com>
2022-05-06 10:55:24 -0400

    Update Kahan

--- SynthMats/Kahan.m
+++ SynthMats/Kahan.m
@@ -1,45 +1,46 @@
-%%% filename: Kahan.m
+% filename: Kahan.m
 
-%%% requires: SubsetSelect.m, PSS.m, PSS_B1.m, PSS_B4.m, PSS_B3.m,
-%%% PSS_srrqr.m, SuccessCheck.m
+% requires: SubsetSelect.m, PSS.m, PSS_B1.m, PSS_B4.m, PSS_B3.m,
+% PSS_srrqr.m, SuccessCheck.m
 
-%%% constructs Kahan matrix realizations and performs column subset selection
+% Main file to construct Kahan matrix realizations and perform CSS
+% from 'Robust Parameter Identifiability Analysis' (Pearce et al. (2022))
 
 clear
 close all
 
 rng default 
 
-Part = struct; %%% main structure to store relevant quantities from each realization
+Part = struct; % main structure to store relevant quantities from each realization
 
-num_reals = 10000; %%% # realizations of Kahan matrix
+num_reals = 10000; % # realizations of Kahan matrix
 num_algs = 4; 
 
-n = 90; %%% dimensions n x n
-k = n-1; %%% num rank
+n = 90; % dimensions n x n
+k = n-1; % num rank
 
-a_zeta = 0.9; %%% lower bound for zeta draws
-b_zeta = 0.99999; %%% upper bound for zeta draws
+a_zeta = 0.9; % lower bound for zeta draws
+b_zeta = 0.99999; % upper bound for zeta draws
 
 for real = 1:num_reals
     if mod(real,25) == 0
         sprintf('Iteration %d \n',real)
     end
 
-    zeta = a_zeta + (b_zeta-a_zeta)*rand; %%% sample zeta
+    zeta = a_zeta + (b_zeta-a_zeta)*rand; % sample zeta
     phi = sqrt(1-zeta^2);
 
     K = eye(n);
     S = zeros(n);
     
-    for row = 1:n %%% construct factors for product S*K
+    for row = 1:n % construct factors for product S*K
         S(row,row) = zeta^(row-1);
         for col = row+1:n
             K(row, col) = -phi;
         end
     end
 
-    [~,~,crit] = SubsetSelect(S*K, [], k, 1:4); %%% not storing UnId or Id but can below
+    [~,~,crit] = SubsetSelect(S*K, [], k, 1:4); % not storing UnId or Id but can below
 
     for alg = 1:num_algs
         Part(real).crit1{alg} = crit{alg}(1,1:2);
@@ -50,12 +51,12 @@ for real = 1:num_reals
         
 end
 
-%%% extract information
+% extract information
 
-crit1_abs = zeros(num_algs,num_reals); %%% criteria 2.4
-crit1_rel = zeros(num_algs,num_reals); %%% gamma_1
-crit2_abs = zeros(num_algs,num_reals); %%% criteria 2.5
-crit2_rel = zeros(num_algs,num_reals); %%% gamma_2
+crit1_abs = zeros(num_algs,num_reals); % criteria 2.4
+crit1_rel = zeros(num_algs,num_reals); % gamma_1
+crit2_abs = zeros(num_algs,num_reals); % criteria 2.5
+crit2_rel = zeros(num_algs,num_reals); % gamma_2
 crit3_cnd = zeros(num_algs,num_reals); %%% cond(S_1)/cond(S)
 
 for real = 1:length(Part)
@@ -68,7 +69,7 @@ for real = 1:length(Part)
     end
 end
 
-%%% compute means for each algorithm
+% compute means for each algorithm
 crit1_abs_mean = mean(crit1_abs,2);
 crit1_rel_mean = mean(crit1_rel,2);
 crit2_abs_mean = mean(crit2_abs,2);
